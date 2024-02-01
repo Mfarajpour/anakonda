f7ab3f4f86d19f4a1a9790f29bc78484ed026262
Mfarajpour <mohsenfarajpour1366@gmail.com>
2024-02-01 16:42:25 +0330

    add test script

---
+++ scripts/test.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+SUCCESS=0
+
+for i in `seq 1 10`; do
+    flask db upgrade &> /dev/null
+    if [ $? -eq 0 ]; then
+       SUCCESS=1
+       break
+    fi
+    
+    s=`expr $i \* 10`
+    echo "INFO: Try $i failed , Going to sleep for $s second"
+    sleep $s
+           
+done
+
+if [ $SUCCESS -eq 0 ]; then
+   echo "ERROR: Could not connect to database"
+   exit 1 
+fi
+
+
+coverage run
+
+coverage html
+
+coverage xml
